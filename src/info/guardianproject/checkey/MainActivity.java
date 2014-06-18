2fa4f15e381a98ccf4642e387f70dbba17671859
Hans-Christoph Steiner <hans@eds.org>
2014-06-18 16:40:49 -0400

    rename layout XML to make Activity class name

--- src/info/guardianproject/checkey/MainActivity.java
+++ src/info/guardianproject/checkey/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_select);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
